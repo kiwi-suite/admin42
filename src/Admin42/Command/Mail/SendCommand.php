c90dddd62c929d42544665ee65a42de5fb7ca3f0
Thomas Payer <payer@raum42.at>
2014-08-20 10:24:19 +0200

    added mailsending

---
+++ src/Admin42/Command/Mail/SendCommand.php
@@ -0,0 +1,184 @@
+<?php
+namespace Admin42\Command\Mail;
+
+use Core42\Command\AbstractCommand;
+use Zend\Mail\Message;
+use Zend\Mime\Part;
+use Zend\View\Model\ViewModel;
+use Zend\View\Renderer\PhpRenderer;
+
+class SendCommand extends AbstractCommand
+{
+    /**
+     * @var Message
+     */
+    private $mailMessage;
+
+    /**
+     * @var ViewModel
+     */
+    private $layout;
+
+    /**
+     * @var ViewModel|string
+     */
+    private $bodyHtml;
+
+    /**
+     * @var ViewModel|string
+     */
+    private $bodyPlain;
+
+    /**
+     *
+     */
+    protected function init()
+    {
+        $this->mailMessage = new Message();
+
+        $this->layout = new ViewModel();
+    }
+
+    /**
+     * @param string $subject
+     * @return $this
+     */
+    public function setSubject($subject)
+    {
+        $this->mailMessage->setSubject($subject);
+
+        return $this;
+    }
+
+    /**
+     * @param string $email
+     * @param null|string $name
+     * @return $this
+     */
+    public function addFrom($email, $name = null)
+    {
+        $this->mailMessage->addFrom($email, $name);
+
+        return $this;
+    }
+
+    /**
+     * @param string $email
+     * @param null|string $name
+     * @return $this
+     */
+    public function addTo($email, $name = null)
+    {
+        $this->mailMessage->addTo($email, $name);
+
+        return $this;
+    }
+
+    /**
+     * @param string $email
+     * @param null|string $name
+     * @return $this
+     */
+    public function addCc($email, $name = null)
+    {
+        $this->mailMessage->addCc($email, $name);
+
+        return $this;
+    }
+
+    /**
+     * @param string $email
+     * @param null|string $name
+     * @return $this
+     */
+    public function addBcc($email, $name = null)
+    {
+        $this->mailMessage->addBcc($email, $name);
+
+        return $this;
+    }
+
+    /**
+     * @param string $email
+     * @param null|string $name
+     * @return $this
+     */
+    public function addReplyTo($email, $name = null)
+    {
+        $this->mailMessage->addReplyTo($email, $name);
+
+        return $this;
+    }
+
+    /**
+     * @param ViewModel|string $bodyHtml
+     * @return $this
+     */
+    public function setBodyHtml($bodyHtml)
+    {
+        $this->bodyHtml = $bodyHtml;
+
+        return $this;
+    }
+
+    /**
+     * @param ViewModel|string $bodyPlain
+     * @return $this
+     */
+    public function setBodyPlain($bodyPlain)
+    {
+        $this->bodyPlain = $bodyPlain;
+
+        return $this;
+    }
+
+    protected function execute()
+    {
+        $viewResolver = $this->getServiceManager()->get('ViewResolver');
+
+        $phpRenderer = new PhpRenderer();
+        $phpRenderer->setResolver($viewResolver);
+
+        $parts = array();
+
+        if ($this->bodyHtml !== null) {
+            if ($this->bodyHtml instanceof ViewModel) {
+                $this->bodyHtml = $phpRenderer->render($this->bodyHtml);
+            }
+            $this->layout->setVariable('content', $this->bodyHtml);
+
+            $this->layout->setTemplate('mail/admin42/layout.html.phtml');
+
+            $htmlPart = new Part($phpRenderer->render($this->layout));
+            $htmlPart->type = 'text/html';
+            $parts[] = $htmlPart;
+            $this->layout->clearVariables();
+        }
+
+        if ($this->bodyPlain !== null) {
+            if ($this->bodyPlain instanceof ViewModel) {
+                $this->bodyPlain = $phpRenderer->render($this->bodyPlain);
+            }
+            $this->layout->setVariable('content', $this->bodyPlain);
+
+            $this->layout->setTemplate('mail/admin42/layout.plain.phtml');
+
+            $plainPart = new Part($phpRenderer->render($this->layout));
+            $plainPart->type = 'text/plain';
+            $parts[] = $plainPart;
+            $this->layout->clearVariables();
+        }
+
+        if (empty($parts)) {
+            return;
+        }
+
+        $body = new \Zend\Mime\Message();
+        $body->setParts($parts);
+
+        $this->mailMessage->setBody($body);
+
+        $transport = $this->getServiceManager()->get('Core42\Mail\Transport');
+        $transport->send($this->mailMessage);
+    }
+}
