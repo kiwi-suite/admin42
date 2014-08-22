3ac00648c6180941d398eb949a1d659f19d88e41
Thomas Payer <payer@raum42.at>
2014-08-22 14:15:10 +0200

    new sidebar

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -49,26 +49,6 @@ var modalHelper = {
 }
 
 $(document).ready(function(){
-    $('#btn-sidebar-hide').click(function(event){
-        $(this).parent().addClass('hide-sidebar');
-        $('#btn-sidebar-show').parent().addClass('hide-sidebar');
-
-        $('.main', $(this).parent().parent())
-            .removeClass('col-md-10')
-            .removeClass('col-md-offset-2')
-            .addClass('col-md-12');
-    });
-
-    $('#btn-sidebar-show').click(function(event){
-        $(this).parent().removeClass('hide-sidebar');
-        $('#btn-sidebar-hide').parent().removeClass('hide-sidebar');
-
-        $('.main', $(this).parent().parent())
-            .removeClass('col-md-12')
-            .addClass('col-md-10')
-            .addClass('col-md-offset-2');
-    });
-
     $('table.table-datatable').on('click', '.delete-list-link', function(event){
         event.preventDefault();
 
