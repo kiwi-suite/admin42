c9835381df8515024951cee8f49e8f571d7ba4ea
Thomas Payer <payer@raum42.at>
2015-05-03 19:34:58 +0200

    added modal controller

---
+++ assets/javascripts/controller/modal.js
@@ -0,0 +1,9 @@
+angular.module('admin42').controller('ModalController', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}]);
