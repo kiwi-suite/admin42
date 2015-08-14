36b5d73ecbb3cfb564055a04d85b1cc7efd8476f
Thomas Payer <payer@raum42.at>
2015-08-14 16:34:37 +0200

    minimalistic dynamic field set

--- config/assets.config.php
+++ config/assets.config.php
@@ -8,4 +8,11 @@ return array(
             'source' => 'module/admin42/assets/dist/',
         ),
     ),
+
+    'assets' => array(
+        __NAMESPACE__ => array(
+            'target' => 'public/assets/admin/rapid',
+            'source' => 'module/rapid/assets/dist/',
+        ),
+    ),
 );
