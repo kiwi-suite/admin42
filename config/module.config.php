2111a93d6ec74a4baa40a784fad36f4570e20243
Thomas Payer <payer@raum42.at>
2015-08-28 23:22:37 +0200

    media events

--- config/module.config.php
+++ config/module.config.php
@@ -53,6 +53,7 @@ return array(
             'Admin42\MediaUrl'         => 'Admin42\Media\Service\MediaUrlFactory',
 
             'Admin42\Crud\EventManager' => 'Admin42\Crud\Service\EventManagerFactory',
+            'Admin42\Media\EventManager' => 'Admin42\Media\Service\EventManagerFactory',
         ),
     ),
 
