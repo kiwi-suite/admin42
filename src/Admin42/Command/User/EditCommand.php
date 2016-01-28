9381bac14b245e7d8c490e8db2ff057b024e93ec
Thomas Payer <payer@raum42.at>
2016-01-28 17:50:35 +0100

    CI

--- src/Admin42/Command/User/EditCommand.php
+++ src/Admin42/Command/User/EditCommand.php
@@ -174,16 +174,16 @@ class EditCommand extends AbstractCommand
         }
 
         if ($this->getTableGateway('Admin42\User')->select(
-                function (Select $select) {
-                    $select->where(
-                        function (Where $where) {
-                            $where->equalTo('email', $this->email);
-                            $where->notEqualTo('id', $this->user->getId());
-                        }
-                    );
-                }
+            function (Select $select) {
+                $select->where(
+                    function (Where $where) {
+                        $where->equalTo('email', $this->email);
+                        $where->notEqualTo('id', $this->user->getId());
+                    }
+                );
+            }
         )->count() > 0) {
-                $this->addError("email", "Email already taken");
+            $this->addError("email", "Email already taken");
         }
 
         if (!empty($this->username)) {
