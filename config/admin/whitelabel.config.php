9343fbc2429e32c83a697d99b35109fe8af516e9
Thomas Payer <payer@raum42.at>
2016-10-16 15:47:24 +0200

    fix: assets path

--- config/admin/whitelabel.config.php
+++ config/admin/whitelabel.config.php
@@ -6,10 +6,10 @@ return [
         'title'                     => 'kiwi42',
         'show-topbar-title'         => true,
         'topbar-title'              => 'kiw<span class="text-r42">i</span>42',
-        'logo-icon'                 => 'assets/admin/admin42/images/logo-icon.png', // topbar -> light
-        'logo-lg'                   => 'assets/admin/admin42/images/logo-lg.png', // login top -> dark
-        'logo-xs'                   => 'assets/admin/admin42/images/logo-icon.png', // sidebar bottom -> light
-        'logo-xs-dark'              => 'assets/admin/admin42/images/logo-lg.png', // login footer -> dark
+        'logo-icon'                 => '/admin/admin42/images/logo-icon.png', // topbar -> light
+        'logo-lg'                   => '/admin/admin42/images/logo-lg.png', // login top -> dark
+        'logo-xs'                   => '/admin/admin42/images/logo-icon.png', // sidebar bottom -> light
+        'logo-xs-dark'              => '/admin/admin42/images/logo-lg.png', // login footer -> dark
         'sidebar-bottom-text'       => 'kiw<span class="text-r42">i</span>42&nbsp;&copy;&nbsp;raum42 OG',
         'sidebar-bottom-link'       => 'https://www.raum42.at',
         'sidebar-bottom-link-title' => 'raum42 OG',
