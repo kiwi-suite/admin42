65fde82e7c3545e49882a5ed0a44c69e89eb6c7d
Thomas Payer <payer@raum42.at>
2016-10-21 13:09:40 +0200

    added travis

---
+++ tests/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * core42
+ *
+ * @package core42
+ * @link https://github.com/raum42/core42
+ * @copyright Copyright (c) 2010 - 2016 raum42 (https://www.raum42.at)
+ * @license MIT License
+ * @author raum42 <kiwi@raum42.at>
+ */
+
+chdir(__DIR__);
+
+date_default_timezone_set("UTC");
+
+$loader = null;
+if (file_exists('../vendor/autoload.php')) {
+    include '../vendor/autoload.php';
+} elseif (file_exists('../../../../vendor/autoload.php')) {
+    include '../../../../vendor/autoload.php';
+}
