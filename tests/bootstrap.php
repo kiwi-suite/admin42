38cccce2a0e5d63b8d4d4419193cfffb7676bb00
Thomas Payer <payer@raum42.at>
2016-10-21 14:40:27 +0200

    fix: file header

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,4 @@
 <?php
-/*
- * core42
- *
- * @package core42
- * @link https://github.com/raum42/core42
- * @copyright Copyright (c) 2010 - 2016 raum42 (https://www.raum42.at)
- * @license MIT License
- * @author raum42 <kiwi@raum42.at>
- */
-
 chdir(__DIR__);
 
 date_default_timezone_set("UTC");
