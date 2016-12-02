caab92ef49d0228a18950b0ee39c6ddf2d33df96
Thomas Payer <payer@raum42.at>
2016-12-02 18:01:08 +0100

    added wysiwyg mutator

--- config/mutator.config.php
+++ config/mutator.config.php
@@ -4,17 +4,21 @@ namespace Admin42;
 use Admin42\Mutator\Strategy\LinkStrategy;
 use Admin42\Mutator\Strategy\Service\LinkStrategyFactory;
 use Admin42\Mutator\Strategy\Service\StackStrategyFactory;
+use Admin42\Mutator\Strategy\Service\WysiwygStrategyFactory;
 use Admin42\Mutator\Strategy\StackStrategy;
+use Admin42\Mutator\Strategy\WysiwygStrategy;
 
 return [
     'mutator' => [
         'factories' => [
             StackStrategy::class                    => StackStrategyFactory::class,
             LinkStrategy::class                     => LinkStrategyFactory::class,
+            WysiwygStrategy::class                  => WysiwygStrategyFactory::class,
         ],
         'aliases' => [
             'stack'                                 => StackStrategy::class,
             'link'                                  => LinkStrategy::class,
+            'wysiwyg'                               => WysiwygStrategy::class,
         ],
     ],
 ];
