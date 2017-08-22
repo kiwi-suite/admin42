a501d7cf2a68aed91ad42729cb479e7914a1098b
Thomas Payer <payer@raum42.at>
2017-08-22 13:15:35 +0200

    repo transfer

--- .php_cs
+++ .php_cs
@@ -3,10 +3,10 @@ $header = <<<'EOF'
 admin42
 
 @package admin42
-@link https://github.com/raum42/admin42
-@copyright Copyright (c) 2010 - 2017 raum42 (https://raum42.at)
+@link https://github.com/kiwi-suite/admin42
+@copyright Copyright (c) 2010 - 2017 kiwi suite (https://www.kiwi-suite.com)
 @license MIT License
-@author raum42 <kiwi@raum42.at>
+@author kiwi suite <tech@kiwi-suite.com>
 EOF;
 return PhpCsFixer\Config::create()
     ->setRiskyAllowed(true)
--- config/admin/whitelabel.config.php
+++ config/admin/whitelabel.config.php
@@ -4,25 +4,25 @@
  * admin42
  *
  * @package admin42
- * @link https://github.com/raum42/admin42
- * @copyright Copyright (c) 2010 - 2017 raum42 (https://raum42.at)
+ * @link https://github.com/kiwi-suite/admin42
+ * @copyright Copyright (c) 2010 - 2017 kiwi suite (https://www.kiwi-suite.com)
  * @license MIT License
- * @author raum42 <kiwi@raum42.at>
+ * @author kiwi suite <tech@kiwi-suite.com>
  */
 
 namespace Admin42;
 
 return [
     'whitelabel' => [
-        'title'                     => 'kiwi42',
+        'title'                     => 'kiwi',
         'show-topbar-title'         => true,
-        'topbar-title'              => 'kiw<span class="text-r42">i</span>42',
+        'topbar-title'              => 'kiw<span class="text-r42">i</span>',
         'logo-icon'                 => '/admin/admin42/images/logo-icon.png', // topbar -> light
         'logo-lg'                   => '/admin/admin42/images/logo-lg.png', // login top -> dark
         'logo-xs'                   => '/admin/admin42/images/logo-icon.png', // sidebar bottom -> light
         'logo-xs-dark'              => '/admin/admin42/images/logo-lg.png', // login footer -> dark
-        'sidebar-bottom-text'       => 'kiw<span class="text-r42">i</span>42&nbsp;&copy;&nbsp;raum42 OG',
-        'sidebar-bottom-link'       => 'https://raum42.at',
-        'sidebar-bottom-link-title' => 'raum42 OG',
+        'sidebar-bottom-text'       => '&copy;&nbsp;kiwi suite GmbH',
+        'sidebar-bottom-link'       => 'https://www.kiwi-suite.com',
+        'sidebar-bottom-link-title' => 'kiwi suite GmbH',
     ],
 ];
